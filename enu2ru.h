636768082d4434d34b1f8d92eb5aa9b66e6effbc
tdtc-hrb <tdtc.hrb@gmail.com>
2026-01-18 20:07:18 -0800

    add enu2ru.h

---
+++ enu2ru.h
@@ -0,0 +1,21 @@
+#ifndef ENU2SLAVIC_RU_H
+#define ENU2SLAVIC_RU_H
+
+class Enu2Russian
+{
+public:
+    /*
+     https://en.openrussian.org/vocab/A1
+     https://en.openrussian.org/vocab/A2
+     https://en.openrussian.org/vocab/B1
+     https://en.openrussian.org/vocab/B2
+     https://en.openrussian.org/vocab/C1
+     https://en.openrussian.org/vocab/C2
+     */
+    QString openrussian[5309][3] = {
+
+    };
+
+};
+
+#endif // ENU2SLAVIC_RU_H
